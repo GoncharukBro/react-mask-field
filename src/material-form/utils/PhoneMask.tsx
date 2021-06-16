cf892e1b8da30f7a54ac01699e37b215aa1b6f07
Nikolay Goncharuk <goncharuk.bro@yandex.ru>
2021-06-16 10:16:49 +1000

    Подправлена маска телефона

--- src/material-form/utils/PhoneMask.tsx
+++ src/material-form/utils/PhoneMask.tsx
@@ -5,10 +5,10 @@ function normalize(value: string, country?: 'ru' | 'other') {
   let maksedValue = '';
 
   if (country === 'ru') {
-    maksedValue = `${value[0] === '8' ? '8' : '+7'} `;
+    maksedValue = `${value[0] === '8' ? '8' : '+7'}`;
 
     if (value.length > 1) {
-      maksedValue += `(${value.substring(1, 4)}`;
+      maksedValue += ` (${value.substring(1, 4)}`;
     }
     if (value.length > 4) {
       maksedValue += `) ${value.substring(4, 7)}`;
@@ -31,62 +31,35 @@ type PhoneMaskProps = any;
 export default function PhoneMask({ ref, onChange, ...other }: PhoneMaskProps) {
   const [normalizeValue, setNormalizeValue] = useState('');
 
-  const handleInput = (event: any) => {
+  const handleChange = (event: any) => {
     // Оставляем только числовые значения
-    let parseValue = event.target.value.replace(/\D/g, '');
-    // Если первый символ - "9", добавляем к значению "7"
-    parseValue = parseValue[0] === '9' ? `7${parseValue}` : parseValue;
-
+    const parseValue = event.target.value.replace(/\D/g, '');
     // Если ввод содержит нечисловые данные, оставляем значение без изменений
     if (!parseValue) {
       const value = '';
       setNormalizeValue(value);
       onChange?.({ ...event, target: { ...event.target, value } });
-    } else if (event.target.value.length !== event.target.selectionStart) {
-      // Если курсор находится в середине поля и введенные символ является нечисловым,
-      // оставляем значение без изменений
+      return;
+    }
+
+    // Проверяем, находится ли курсор в середине поля
+    if (event.target.value.length !== event.target.selectionStart) {
+      // Если введенные символ является нечисловым, оставляем значение без изменений
+      // При "Backspace" event.data принимает значение `null`
       if (event.data && /\D/g.test(event.data)) {
         const value = parseValue;
         setNormalizeValue(value);
         onChange?.({ ...event, target: { ...event.target, value } });
       }
-    } else {
-      // Определяем по первому символу к какой стране принадлежит номер
-      const country = ['7', '8', '9'].includes(parseValue[0]) ? 'ru' : undefined;
-      const value = normalize(parseValue, country);
-      setNormalizeValue(value);
-      onChange?.({ ...event, target: { ...event.target, value } });
+      return;
     }
-  };
 
-  const handleKeyDown = (event: any) => {
-    // Оставляем только числовые значения
-    const parseValue = event.target.value.replace(/\D/g, '');
-    // Очищаем поле после удаления последнего символа
-    if (parseValue?.length === 1 && event.key === 'Backspace') {
-      const value = '';
-      setNormalizeValue(value);
-      onChange?.({ ...event, target: { ...event.target, value } });
-    }
-  };
-
-  const handlePaste = (event: any) => {
-    // Достаем данные из буфера
-    const clipboardData = event.clipboardData.getData('Text');
-    // Присваиваем только числовые значения,
-    // на случай если в буфере присутсвуют нечисловые значения
-    const value = clipboardData.replace(/\D/g, '');
+    // Определяем по первому символу к какой стране принадлежит номер
+    const country = ['7', '8', '9'].includes(parseValue[0]) ? 'ru' : undefined;
+    const value = normalize(parseValue[0] === '9' ? `7${parseValue}` : parseValue, country);
     setNormalizeValue(value);
     onChange?.({ ...event, target: { ...event.target, value } });
   };
 
-  return (
-    <input
-      {...other}
-      value={normalizeValue}
-      onInput={handleInput}
-      onKeyDown={handleKeyDown}
-      onPaste={handlePaste}
-    />
-  );
+  return <input {...other} value={normalizeValue} onChange={handleChange} />;
 }

391bc1874fbc7376e41da0a88fb700197adede41
Nikolay Goncharuk <goncharuk.bro@yandex.ru>
2021-07-01 00:03:11 +1000

    Add test

---
+++ src/masked-input/MaskedInput.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MaskedInput from './MaskedInput';
+
+const maskedInput = <MaskedInput mask="+7 (___) ___-__-__" char="_" />;
+
+function getInputElement() {
+  return screen.getByRole('textbox') as HTMLInputElement;
+}
+
+test('type', () => {
+  render(maskedInput);
+
+  const input = getInputElement();
+  userEvent.type(input, '9123456789');
+  expect(input).toHaveValue('+7 (912) 345-67-89');
+});

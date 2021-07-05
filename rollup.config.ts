b1a6cea63f3d45d5ab0c877fcc52eb0ebd504029
Nikolay Goncharuk <goncharuk.bro@yandex.ru>
2021-07-05 22:06:07 +1000

    Add rollup.config.ts

---
+++ rollup.config.ts
@@ -0,0 +1,32 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: 'dist',
+      format: 'cjs',
+      exports: 'named',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    babel({
+      babelHelpers: 'bundled',
+    }),
+    terser(),
+  ],
+  external: ['tslib'],
+};
